9dec8272705544a134c03e312cb965cefd0711fb
gky360 <gky360@gmail.com>
2018-05-23 00:37:40 +0900

    use unix domain socket for mysql connection

--- app.go
+++ app.go
@@ -729,18 +729,18 @@ func loggingMiddleware(next http.Handler) http.Handler {
 }
 
 func main() {
-	host := os.Getenv("ISUCON5_DB_HOST")
-	if host == "" {
-		host = "localhost"
-	}
-	portstr := os.Getenv("ISUCON5_DB_PORT")
-	if portstr == "" {
-		portstr = "3306"
-	}
-	port, err := strconv.Atoi(portstr)
-	if err != nil {
-		log.Fatalf("Failed to read DB port number from an environment variable ISUCON5_DB_PORT.\nError: %s", err.Error())
-	}
+	// host := os.Getenv("ISUCON5_DB_HOST")
+	// if host == "" {
+	// 	host = "localhost"
+	// }
+	// portstr := os.Getenv("ISUCON5_DB_PORT")
+	// if portstr == "" {
+	// 	portstr = "3306"
+	// }
+	// port, err := strconv.Atoi(portstr)
+	// if err != nil {
+	// 	log.Fatalf("Failed to read DB port number from an environment variable ISUCON5_DB_PORT.\nError: %s", err.Error())
+	// }
 	user := os.Getenv("ISUCON5_DB_USER")
 	if user == "" {
 		user = "root"
@@ -755,7 +755,8 @@ func main() {
 		ssecret = "beermoris"
 	}
 
-	db, err = sql.Open("mysql", user+":"+password+"@tcp("+host+":"+strconv.Itoa(port)+")/"+dbname+"?loc=Local&parseTime=true")
+	var err error
+	db, err = sql.Open("mysql", user+":"+password+"@unix(/var/run/mysqld/mysqld.sock)/"+dbname+"?loc=Local&parseTime=true")
 	if err != nil {
 		log.Fatalf("Failed to connect to DB: %s.", err.Error())
 	}
